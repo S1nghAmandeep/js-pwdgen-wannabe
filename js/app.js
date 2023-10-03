4733f1ccf40bdbe4f4002decfa3776113c25bc25
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-03 16:13:01 +0200

    added-password-number-variable

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
 // # Password generator
 
 // - chiedere all'utente il suo nome
-    // - inserisci il tuo nome
+    // - dichiarare una variabile per nome
 let userName
+
 //     - assegnare alla variabile il valore ricevuto
 userName = prompt('Ciao, Inserisci il tuo nome?')
-console.log(userName)    
+console.log(userName)
+
 // - chiedere il suo cognome
-//     - inserisci il tuo cognome
+//     - dichiarare una variabile per cognome
 let userSurname
+
 //     - assegnare alla variabile il valore ricevuto
 userSurname = prompt('Inserisci il tuo Cognome?')
 console.log(userSurname)
+
 // - chiedere il suo colore preferito
-//     - inserici il tuo colore preferito
+//     - dichiarare una variabile per colore preferito
 let favoriteColour
+
 //     - assegnare alla variabile il valore ricevuto
 favoriteColour = prompt('Inserisci il tuo colore preferito')
 console.log(favoriteColour)
+
+// - inserire il numero per creare la password
+    // - assegnare alla variabile il numero
+let numberForPassword = (21)
+console.log(numberForPassword)
+
 // - concatenazione del risultato ricevuto
-let message = userName + userSurname + favoriteColour
+//      - dichirare una varialbile con concatenazione con dati ricevuti
+let message = userName + userSurname + favoriteColour + numberForPassword
 console.log(message)
